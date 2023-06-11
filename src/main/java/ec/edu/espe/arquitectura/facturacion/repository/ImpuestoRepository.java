eb456614952186d3ce7b23fb61c3c43d7263be14
Jerry Tovar <65546801+joetovar@users.noreply.github.com>
2023-06-10 22:03:49 -0500

    Update ImpuestoRepository
    Jerry Tovar

--- src/main/java/ec/edu/espe/arquitectura/facturacion/repository/ImpuestoRepository.java
+++ src/main/java/ec/edu/espe/arquitectura/facturacion/repository/ImpuestoRepository.java
@@ -2,8 +2,13 @@ package ec.edu.espe.arquitectura.facturacion.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import ec.edu.espe.arquitectura.facturacion.model.Cliente;
 import ec.edu.espe.arquitectura.facturacion.model.Impuesto;
 
+import java.util.List;
+
 public interface ImpuestoRepository extends JpaRepository<Impuesto, String> {
-    
+
+    List<Impuesto> findByNombreLikeOrderByNombre(String nombrePattern);
+
 }
